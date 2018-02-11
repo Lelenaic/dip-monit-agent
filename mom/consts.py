b39d602b35d27cc0764ef1d930147f8c51ef027d
Lénaïc Grolleau <lenaic@lenaic.me>
2018-02-11 15:17:17 +0100

    Changed backend protocol

--- mom/consts.py
+++ mom/consts.py
@@ -3,6 +3,6 @@ import os
 
 class Consts:
     MOM_HOME = os.path.dirname(os.path.abspath(__file__))[:-4]
-    MAIN_SERVER_API_URL = "http://mom.dip.lenaic.me/api/"
+    MAIN_SERVER_API_URL = "https://mom.dip.lenaic.me/api/"
     #MAIN_SERVER_API_URL = "http://localhost:5000/api/"
     MOM_SETTINGS_FILE = MOM_HOME + os.path.sep + "settings.yaml"
